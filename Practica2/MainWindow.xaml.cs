b512e67c7f9169a2d292dd070bb6e4557ff6353c
staakumi <s.ilin2036@gmail.com>
2024-03-21 14:46:28 +0300

    Добавьте файлы проекта.

---
+++ Practica2/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Practica2.Hobby_shopDataSetTableAdapters;
+
+
+namespace Practica2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        Shop_EmployeesTableAdapter employees = new Shop_EmployeesTableAdapter();
+        ProductsTableAdapter products = new ProductsTableAdapter();
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+            EmployeesDataGrid.ItemsSource = employees.GetData();
+
+
+            ProductsDataGrid.ItemsSource = products.GetData();
+
+
+        }
+
+        public void ShowEmployees_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SurnameTbx.Visibility = Visibility.Visible;
+            NameTbx.Visibility = Visibility.Visible;
+            MiddleNameTbx.Visibility = Visibility.Visible;
+            AgeTbx.Visibility = Visibility.Visible;
+
+            
+            NamePro.Visibility = Visibility.Collapsed;
+            PricePro.Visibility = Visibility.Collapsed;
+
+            EmployeesDataGrid.Visibility = Visibility.Visible;
+            ProductsDataGrid.Visibility = Visibility.Collapsed;
+        }
+        public void ShowProducts_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SurnameTbx.Visibility = Visibility.Collapsed;
+            NameTbx.Visibility = Visibility.Collapsed;
+            MiddleNameTbx.Visibility = Visibility.Collapsed;
+            AgeTbx.Visibility = Visibility.Collapsed;
+
+            
+            NamePro.Visibility = Visibility.Visible;
+            PricePro.Visibility = Visibility.Visible;
+
+            EmployeesDataGrid.Visibility = Visibility.Collapsed;
+            ProductsDataGrid.Visibility = Visibility.Visible;
+        }
+
+
+        private void OpenNewWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window = new Window2();
+            window.Show();
+        }
+
+        private void ButtonInsertClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDataGrid.Visibility == Visibility.Visible)
+            {
+                employees.InsertQuery( NameTbx.Text, SurnameTbx.Text, MiddleNameTbx.Text, Convert.ToInt32(AgeTbx.Text));
+                EmployeesDataGrid.ItemsSource = employees.GetData();
+            }
+            else
+            {
+                products.InsertQueryPro( NamePro.Text, Convert.ToInt32(PricePro.Text));
+                ProductsDataGrid.ItemsSource = products.GetData();
+            }
+        }
+        private void ButtonUpdateClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDataGrid.Visibility == Visibility.Visible)
+            {
+                object ID1 = (EmployeesDataGrid.SelectedItem as DataRowView).Row[0];
+                employees.UpdateQuery(NameTbx.Text, SurnameTbx.Text, MiddleNameTbx.Text, Convert.ToInt32(AgeTbx.Text), Convert.ToInt32(ID1));
+                EmployeesDataGrid.ItemsSource = employees.GetData();
+            }
+            else
+            {
+                object ID2 = (ProductsDataGrid.SelectedItem as DataRowView).Row[0];
+                products.UpdateQueryPro(NamePro.Text, Convert.ToInt32(PricePro.Text), Convert.ToInt32(ID2));
+                ProductsDataGrid.ItemsSource = products.GetData();
+            }
+        }
+
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDataGrid.Visibility == Visibility.Visible)
+            {
+                object ID1 = (EmployeesDataGrid.SelectedItem as DataRowView).Row[0];
+                employees.DeleteQuery(Convert.ToInt32(ID1));
+                EmployeesDataGrid.ItemsSource = employees.GetData();
+            }
+            else
+            {
+                object ID2 = (ProductsDataGrid.SelectedItem as DataRowView).Row[0];
+                products.DeleteQueryPro(Convert.ToInt32(ID2));
+                ProductsDataGrid.ItemsSource = products.GetData();
+            }
+        }
+
+    }
+
+}
+
---
+++ Practica2/Window2.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private Hobby_shopEntities context = new Hobby_shopEntities();
+
+
+        public Window2()
+        {
+            InitializeComponent();
+            EmployeesDgr.ItemsSource = context.Shop_Employees.ToList();
+
+
+
+            ProductsDgr.ItemsSource = context.Products.ToList();
+
+
+
+
+
+        }
+        private void ShowEmployees_ClickEF(object sender, RoutedEventArgs e)
+        {
+
+            SurnameTbx.Visibility = Visibility.Visible;
+            NameTbx.Visibility = Visibility.Visible;
+            MiddleNameTbx.Visibility = Visibility.Visible;
+            AgeTbx.Visibility = Visibility.Visible;
+
+
+            NamePro.Visibility = Visibility.Collapsed;
+            PricePro.Visibility = Visibility.Collapsed;
+
+            EmployeesDgr.Visibility = Visibility.Visible;
+            ProductsDgr.Visibility = Visibility.Collapsed;
+        }
+        private void ShowProducts_ClickEF(object sender, RoutedEventArgs e)
+        {
+
+            SurnameTbx.Visibility = Visibility.Collapsed;
+            NameTbx.Visibility = Visibility.Collapsed;
+            MiddleNameTbx.Visibility = Visibility.Collapsed;
+            AgeTbx.Visibility = Visibility.Collapsed;
+
+
+            NamePro.Visibility = Visibility.Visible;
+            PricePro.Visibility = Visibility.Visible;
+
+            EmployeesDgr.Visibility = Visibility.Collapsed;
+            ProductsDgr.Visibility = Visibility.Visible;
+        }
+        private void ButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDgr.Visibility == Visibility.Visible)
+            {
+
+                Shop_Employees emp = new Shop_Employees();
+
+                emp.Employees_Last_Name = SurnameTbx.Text;
+                emp.Employees_First_Name = NameTbx.Text;
+                emp.Employees_Middle_Name = MiddleNameTbx.Text;
+                emp.Employees_Age = Convert.ToInt32(AgeTbx.Text);
+                context.Shop_Employees.Add(emp);
+                context.SaveChanges();
+                EmployeesDgr.ItemsSource = context.Shop_Employees.ToList();
+
+            }
+            else
+            {
+                Products emp2 = new Products();
+
+                emp2.Products_Name = NamePro.Text;
+                emp2.Products_Price = Convert.ToInt32(PricePro.Text);
+                context.Products.Add(emp2);
+                context.SaveChanges();
+                ProductsDgr.ItemsSource = context.Products.ToList();
+            }
+        }
+        private void ButtonChangeClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDgr.SelectedItem != null)
+            {
+                var selected = EmployeesDgr.SelectedItem as Shop_Employees;
+
+                selected.Employees_First_Name = NameTbx.Text;
+                selected.Employees_Last_Name = SurnameTbx.Text;
+                selected.Employees_Middle_Name = MiddleNameTbx.Text;
+                selected.Employees_Age = Convert.ToInt32(AgeTbx.Text);
+
+                context.SaveChanges();
+                EmployeesDgr.ItemsSource = context.Shop_Employees.ToList();
+            }
+            if (ProductsDgr.SelectedItem != null)
+            {
+                var selectedPro = ProductsDgr.SelectedItem as Products;
+
+                selectedPro.Products_Name = NamePro.Text;
+                selectedPro.Products_Price = Convert.ToInt32(PricePro.Text);
+
+                context.SaveChanges();
+                ProductsDgr.ItemsSource = context.Products.ToList();
+            }
+        }
+        private void ButtonRemoveClick(object sender, RoutedEventArgs e)
+        {
+            if (EmployeesDgr.SelectedItem != null)
+            {
+                context.Shop_Employees.Remove(EmployeesDgr.SelectedItem as Shop_Employees);
+
+                context.SaveChanges();
+                EmployeesDgr.ItemsSource = context.Shop_Employees.ToList();
+            }
+            if (ProductsDgr.SelectedItem != null)
+            {
+                context.Products.Remove(ProductsDgr.SelectedItem as Products);
+
+                context.SaveChanges();
+                ProductsDgr.ItemsSource = context.Products.ToList();
+            }
+
+        }
+    }
+}
